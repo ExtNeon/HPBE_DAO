88656cf7c4112bb6e1503d400230f857f1b5362b
ExtNeon <michael.ftsq13@gmail.com>
2020-11-27 23:11:08 +0300

    small changes

--- engine/core_methods.php
+++ engine/core_methods.php
@@ -670,3 +670,11 @@ function isBot($user_agent)
 
     return false;
 }
+function removeDirectory($dir) {
+    if ($objs = glob($dir."/*")) {
+       foreach($objs as $obj) {
+         is_dir($obj) ? removeDirectory($obj) : unlink($obj);
+       }
+    }
+    rmdir($dir);
+  }
